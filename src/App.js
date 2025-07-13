574d42c01e032d0721f34c2daee1d114e5d16c6a
Greatteacherjubro <wisdomjh100@gmail.com>
2025-07-13 23:09:50 +0900

    전개 연산자, Alert 생성

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@ import { useState } from "react";
 import "./App.css";
 import ExpenseForm from './components/ExpenseForm'
 import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
 
 
 const App = () => {
+
+  const [alert, setAlert] = useState({ show: false });
+
+  const [charge, setCharge] = useState("");
+
+  const [amount, setAmount] = useState(0);
+
+  const handleCharge = (e) =>{
+    
+    setCharge(e.target.value);
+  }
+
+  const handleAmount = (e) =>{
+    setAmount(e.target.valueAsNumber);
+  }
+
+  const handleAlert = ({type,text}) => {
+    setAlert({show: true, type, text});
+    setTimeout(() => {
+      setAlert({show: true});
+    }, 7000);
+  }
 
   const [expenses, setExpense] = useState([
     {
@@ -23,21 +46,46 @@ const App = () => {
     }
   ])
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0){
+      const newExpense = {id: crypto.randomUUID(), charge, amount}
+      const newExpenses = [...expenses, newExpense];
+      setExpense(newExpenses);
+      setCharge('');
+      setAmount(0);
+      handleAlert({type: 'success', text:'아이템이 생성되었습니다.'});
+    }
+
+    else{
+      handleAlert({type: 'danger', text:'charge는 빈값일 수 없으며 amount는 0보다 커야합니다'});
+      console.log('error');
+    }
+  }
+
+
 
   const handleDelete = (id) => {
     const newExpenses = expenses.filter(expense => expense.id !== id)
     console.log(newExpenses);
     setExpense(newExpenses);
+    handleAlert({type: 'danger', text: '아이템이 삭제 되었습니다.'});
   }
 
     return(
       <main className="main-container">
+        {alert.show ? <Alert type={alert.type} text={alert.text} /> : null}
         <h1>예산 계산기</h1>
         
         <div style={{ width:'100%', backgroundColor : 'white', padding : '1rem'}}>
         { /* expense Form*/ }
-        <ExpenseForm />
+        <ExpenseForm 
+          handleCharge = {handleCharge}
+          handleAmount = {handleAmount}
+          charge = {charge}
+          amount = {amount}
+          handleSubmit = {handleSubmit}
+        />
         </div>
 
         <div style={{ width:'100%', backgroundColor : 'white', padding : '1rem'}}>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,37 +2,43 @@ import React from 'react'
 import "./ExpenseForm.css";
 import { MdSend } from 'react-icons/md';
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ handleCharge, charge, handleAmount, amount, handleSubmit }) => {
 
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className='form-center'>
 
           <div className='form-group'>
             <label htmlFor='charge'>지출 항목</label>
             <input 
-            type='text' 
-            className='form-control' 
-            id='charge' 
-            name='charge' 
-            placeholder='예) 렌트비'
+              type='text' 
+              className='form-control' 
+              id='charge' 
+              name='charge'
+              value={charge}
+              placeholder='예) 렌트비'
+              onChange={handleCharge}
             />
           </div>
 
           <div className='form-group'>
             <label htmlFor='amount'>지출 비용</label>
             <input 
-            type='number' 
-            className='form-control' 
-            id='amount' 
-            name='amount' 
-            placeholder='예) 100원'
+              type='number' 
+              className='form-control' 
+              id='amount' 
+              name='amount'
+              value={amount} 
+              placeholder='예) 100원'
+              onChange={handleAmount}
             />
           </div>
         </div>
         <button type='submit' className='btn'>
           제출
-          <MdSend className='btn-icon' />
+          <MdSend className='btn-icon' 
+
+          />
         </button>
       </form>
     )
